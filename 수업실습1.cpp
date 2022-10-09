5c8424deb71fa9b60b5aae17b8dac0c1843a8718
chmpgnesupernova <101654598+chmpgnesupernova@users.noreply.github.com>
2022-10-09 16:12:51 +0900

    Add files via upload

---
+++ 수업실습1.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+
+	int maxValue;
+	cin >> maxValue;
+	int dividedValue = 2;
+	int divisor = 2;
+	bool isPrime = true;
+
+	while (dividedValue <= maxValue) {
+		isPrime = true;
+
+		while (divisor < dividedValue) {
+
+			if (dividedValue % divisor == 0) {
+				isPrime = false;
+				break;
+			}
+
+			divisor++;
+		}
+		divisor = 2;
+
+		if (isPrime) {
+			cout << dividedValue << endl;
+		}
+
+		dividedValue++;
+	}
+
+
+}
